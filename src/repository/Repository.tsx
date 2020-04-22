2f822f8a4abb8bf68539ab0384e33cbb68a63b70
thiagodevx <thiago.dev.x@gmail.com>
2020-04-22 02:30:45 -0300

    evolução do repository

--- src/repository/Repository.tsx
+++ src/repository/Repository.tsx
@@ -1,10 +1,92 @@
-import React from 'react'
-import { useRouteMatch } from 'react-router-dom'
-
+import React, { useEffect, useState } from 'react'
+import { useRouteMatch, Link } from 'react-router-dom'
+import { Header, RepositoryInfo, Issues } from './Repository.style'
+import logoImage from '../assets/logo.svg'
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
+import api from '../app.api'
 interface RepositoryParams {
   repositoryName: string
 }
+interface RepositoryInterface {
+  full_name: string
+  description: string
+  stargazers_count: number
+  forks_count: number
+  open_issues_count: number
+  owner: {
+    login: string
+    avatar_url: string
+  }
+}
+const initialStateRepository: RepositoryInterface = {
+  full_name: '',
+  description: '',
+  stargazers_count: 0,
+  forks_count: 0,
+  open_issues_count: 0,
+  owner: {
+    login: '',
+    avatar_url: ''
+  }
+}
+interface IssueInterface {
+  title: string
+  id: string
+  user: {
+    login: string
+  }
+}
 export default () => {
   const { params } = useRouteMatch<RepositoryParams>()
-  return <h1>Repository: {params.repositoryName} </h1>
+  const [repository, setRepository] = useState<RepositoryInterface>(initialStateRepository)
+  const [issues, setIssues] = useState<IssueInterface[]>([])
+
+  useEffect(() => {
+    api.get(`repos/${params.repositoryName}`).then(response => setRepository(response.data))
+    api.get(`repos/${params.repositoryName}/issues`).then(response => setIssues(response.data))
+  }, [params.repositoryName])
+
+  return (
+    <React.Fragment>
+      <Header>
+        <img src={logoImage} alt='Github Explorer'></img>
+        <Link to='/dashboard'>
+          <FiChevronLeft size={16}></FiChevronLeft>
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img src='https://avatars0.githubusercontent.com/u/28929274?v=4' alt='imagem do repositório'></img>
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>descrição do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to=''>
+          <div>
+            <strong>asassadasdsa</strong>
+            <p>asfsadasd</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </React.Fragment>
+  )
 }
